bf08053db061cb469d38402272826616d3d007a4
jfranklyn <jaf7034@gmail.com>
2020-06-05 17:30:08 -0500

    updated 06/06/20 fixing lot of bugglies

--- PhPCode/pihydropdata-database.php
+++ PhPCode/pihydropdata-database.php
@@ -1,12 +1,5 @@
 <!--
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/cloud-weather-station-esp32-esp8266/
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+John Franklyn 06/06/20
 -->
 <?php
   $servername = "localhost";
